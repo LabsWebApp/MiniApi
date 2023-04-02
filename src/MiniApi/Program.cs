3b43d909046fd5832069662d9bf2c4ea1df47550
Vladimir <vovikdoc@gmail.com>
2023-04-02 09:13:03 +0700

    Добавили БД (SqLite)

--- src/MiniApi/Program.cs
+++ src/MiniApi/Program.cs
@@ -1,30 +1,71 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApiDb>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
+
 var app = builder.Build();
 
-var users = new List<User>();
-
-app.MapGet("/users", () => users);
-app.MapGet("/users/{Id}", (int id) => users.FirstOrDefault(u => u.Id == id));
-
-app.MapPost("/users", (User user) => users.Add(user));
-
-app.MapPut("/users", (User user) =>
+if (app.Environment.IsDevelopment())
 {
-    var index = users.FindIndex(u => u.Id == user.Id);
-    if (index < 0) throw new Exception("Not found");
-    users[index] = user;
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<ApiDb>();
+    db.Database.EnsureCreated();
+}
+
+app.MapGet("/users", async (ApiDb db) => await db.Users.ToListAsync());
+app.MapGet("/users/{Id}", async (int id, ApiDb db) =>
+    await db.Users.FirstOrDefaultAsync(u => u.Id == id) is { } user
+        ? Results.Ok(user)
+        : Results.NotFound());
+
+//app.MapPost("/users", 
+//    async ([FromBody]User user, /*[FromServices]*/ApiDb db, HttpResponse response) =>
+//{
+//    db.Users.Add(user);
+//    await db.SaveChangesAsync();
+//    response.StatusCode = 201;
+//    response.Headers.Location = $"/users/{user.Id}";
+//});
+
+app.MapPost("/users", async ([FromBody] User user, ApiDb db) =>
+    {
+        db.Users.Add(user);
+        await db.SaveChangesAsync();
+        return Results.Created($"/users/{user.Id}", user);
+    });
+
+app.MapPut("/users", async ([FromBody] User user, ApiDb db) =>
+{
+    var oldUser = await db.Users.FindAsync(user.Id);
+    if (oldUser == null) return Results.NotFound();
+    if (oldUser.Name != user.Name) oldUser.Name = user.Name;
+    if (oldUser.Age != user.Age) oldUser.Age = user.Age;
+    if (Math.Abs(oldUser.Ratio - user.Ratio) > 0.0001) oldUser.Ratio = user.Ratio;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
-app.MapDelete("/users/{Id}", (int id) =>
+app.MapDelete("/users/{Id}", async (int id, ApiDb db) =>
 {
-    var index = users.FindIndex(u => u.Id == id);
-    if (index < 0) throw new Exception("Not found");
-    users.RemoveAt(index);
+    var oldUser = await db.Users.FindAsync(id);
+    if (oldUser == null) return Results.NotFound();
+    db.Users.Remove(oldUser);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
+
+// Конвейер обработки запроса - middleware 
+app.UseHttpsRedirection();
 
 app.Run();
 
 
+public class ApiDb : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+
+    public ApiDb(DbContextOptions<ApiDb> options) : base(options){}
+}
 
 public class User
 {
